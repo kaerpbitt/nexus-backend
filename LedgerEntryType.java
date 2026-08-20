5b7c5dafd4afd4a00cb5321073fcd563a827dd8b
kaerpbitt <kaerpbitt@gmail.com>
2026-08-21 02:23:04 +0700

    Create LedgerEntryType.java

---
+++ LedgerEntryType.java
@@ -0,0 +1,11 @@
+package com.financial.engine.enums;
+
+public enum LedgerEntryType {
+    DEPOSIT,
+    WITHDRAWAL,
+    MARGIN_LOCK,
+    MARGIN_RELEASE,
+    TRADE_PROFIT,
+    TRADE_LOSS,
+    FEE_DEDUCTION
+}
